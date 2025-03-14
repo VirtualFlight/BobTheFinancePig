4e9b26a1785abb88791af5c730b3407288f13adf
Jefferson Chen <jefferson8268@hotmail.com>
2025-03-14 12:29:31 -0400

    added login button component

---
+++ bob-the-finance-pig/components/LoginButton.tsx
@@ -0,0 +1,36 @@
+import {View, Text, StyleSheet, Pressable} from 'react-native';
+
+export default function LoginButton({borderColor, backgroundColor, textColor, textMessage}) {
+  return (
+
+    <Pressable style={[styles.loginButtonContainer, {borderColor, backgroundColor}]}>
+        <Text style={[styles.text, {color: textColor}]}> {textMessage} </Text>
+    </Pressable>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  text:{
+    fontSize: 16,
+  },
+
+  loginButtonContainer:{
+    color: 'white',
+    height: 64,
+    borderWidth: 2,
+    borderRadius: 10,
+    // borderColor: 'white',
+    backgroundColor: 'white', 
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    // padding: 10,
+  },
+
+
+
+  
+
+  
+});
