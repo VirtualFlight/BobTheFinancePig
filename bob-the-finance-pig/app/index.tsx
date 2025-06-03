0edd072ec8c00c7fdc718b91f2d1838a6e5eb8b3
tharuk999 <tharukran@gmail.com>
2025-06-02 20:07:32 -0400

    added layout to tracker + fixed some of login

--- bob-the-finance-pig/app/index.tsx
+++ bob-the-finance-pig/app/index.tsx
@@ -9,41 +9,51 @@ import "../global.css"
 export default function Index() {
   return (
     
-    <LinearGradient
-      colors={['#2E025D', '#000000', '#2E025D']}
-      locations={[0, 0.5, 0.7, 1]} // Matches the percentages in your gradient
-      >
-    <View className="px-3 h-full gap-4">
+    // <LinearGradient
+    //   colors={['#2E025D', '#000000', '#2E025D']}
+    //   locations={[0, 0.5, 0.7, 1]} // Matches the percentages in your gradient
+    //   className="p-4"
+    //   >
+    <ScrollView className="p-4">
 
-      
-      <StatusBar backgroundColor="transparent" translucent />
-      {/* Change the gradient later */}
-      <View className="h-3/4 bg-gradient-to-b from-[#2E025D] from-0% via-[#000000] via-60%  to-[#2E025D] to-100% justify-center items-center">
-        <Image source={require('@/assets/images/bob-image.png')}/>
+    
+      <View className="h-full gap-8 ">
+
+        
+        <StatusBar backgroundColor="transparent" translucent />
+        {/* Change the gradient later */}
+        <View className=" h-full bg-gradient-to-b from-[#2E025D] from-0% via-[#000000] via-50%  to-[#2E025D] to-100% justify-center items-center">
+          <Image source={require('@/assets/images/bob-image.png')}/>
+        </View>
+
+      {/* <LinearGradient
+        colors={['#2E025D', '#000000', '#2E025D']}
+        locations={[0, 0.6, 1]} // Matches the percentages in your gradient
+        style={{
+          flex: 1,
+          borderWidth: 4,
+          // marginTop: 3,
+          // borderColor: '#9333ea', // purple-400
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        >
+        <Image source={require('@/assets/images/bob-image.png')} />
+        </LinearGradient> */}
+        <View className="gap-4 ">
+
+
+          <LoginButton borderColor={'white'} backgroundColor={'white'} textColor={'black'} textMessage={'Login'} path={'/main'}></LoginButton>
+          <LoginButton borderColor={'white'} backgroundColor={'black'} textColor={'white'} textMessage={'Register'} path={'/main'}></LoginButton>
+        </View>
+
+        {/* not responsive, change later */}
+        <View className="w-full justify-center items-center">
+          <Text className="color-white text-xs">Copyright © bob the finance guy</Text>
+        </View>
       </View>
-
-    {/* <LinearGradient
-      colors={['#2E025D', '#000000', '#2E025D']}
-      locations={[0, 0.6, 1]} // Matches the percentages in your gradient
-      style={{
-        flex: 1,
-        borderWidth: 4,
-        // marginTop: 3,
-        // borderColor: '#9333ea', // purple-400
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-      >
-      <Image source={require('@/assets/images/bob-image.png')} />
-      </LinearGradient> */}
-      <View className="gap-4">
-
-
-        <LoginButton borderColor={'white'} backgroundColor={'white'} textColor={'black'} textMessage={'Login'} path={'/main'}></LoginButton>
-        <LoginButton borderColor={'white'} backgroundColor={'black'} textColor={'white'} textMessage={'Register'} path={'/main'}></LoginButton>
-      </View>
-    </View>
-  </LinearGradient>
+    </ScrollView>
+  // </LinearGradient>
   );
 
 
--- bob-the-finance-pig/app/tracker.tsx
+++ bob-the-finance-pig/app/tracker.tsx
@@ -11,11 +11,23 @@ export default function Tracker() {
       <View className="h-full items-center py-10">
         <Header/>
         <View className="mt-20 w-full h-full border-2 border-red-500">
-            {/* <View className="w-full h-1/3 bg-[#ffffff]"></View>
-            <View className="w-full h-1/3 bg-[#ffffff]"></View>
-            <View className="w-full h-1/3 bg-[#ffffff]"></View> */}
+            {/* <View className="w-full h-1/3 bg-[#ffffff]"></View> */}
             <View className="w-full justify-center items-center">
-                <Text className="text-white ">Expense Tracker</Text>
+                <Text className="text-white text-3xl">Febuary 28 2025</Text>
+                {/* <Text className="text-white ">Expense Tracker</Text> */}
+            </View>
+            <View className="w-full h-1/6 border-2 border-red-400 justify-center items-center gap-4">
+                <View className="w-5/6 h-8 border-2 border-red-400 rounded-xl"></View>
+                <View className="w-5/6 h-8 border-2 border-red-400 rounded-xl"></View>
+            </View>
+            <View className="p-4 w-full h-1/2">
+                <View className="flex-row">
+                    <View className="w-5/6 h-8 border-2 border-red-400 rounded-xl"></View>
+                    <View className="w-1/6 h-8 border-2 border-red-400 rounded-xl"></View>
+                </View>
+                <View className="w-full h-full border-2 border-blue-400">
+                    <View></View>
+                </View>
             </View>
 
 
